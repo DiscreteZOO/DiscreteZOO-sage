af39a9b2b5814f6a07469b4175d86b66d6ffa0fe
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2016-01-26 13:54:11 +0100

    Add aliases for derived fields

--- discretezoo/entities/zoograph/__init__.py
+++ discretezoo/entities/zoograph/__init__.py
@@ -5,3 +5,14 @@ from ..zootypes import init_class
 import fields
 
 init_class(ZooGraph, fields)
+fields.density = 2 * fields.size / (fields.order * (fields.order - 1))
+fields.has_loops = fields.number_of_loops != 0
+fields.is_half_transitive = fields.is_edge_transitive & \
+                            fields.is_vertex_transitive & \
+                            ~fields.is_arc_transitive
+fields.is_semi_symmetric = fields.is_regular & \
+                           fields.is_edge_transitive & \
+                           ~fields.is_vertex_transitive
+fields.is_triangle_free = fields.triangles_count == 0
+fields.is_weakly_chordal = fields.is_long_hole_free & \
+                           fields.is_long_antihole_free
