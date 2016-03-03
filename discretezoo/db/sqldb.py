9d8309544772a10e2705e39aa7b86d4be62c9bfd
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2016-03-03 18:59:26 +0100

    Add documentation for the generic SQL database interface

--- discretezoo/db/sqldb.py
+++ discretezoo/db/sqldb.py
@@ -1,3 +1,10 @@
+r"""
+SQL database interface
+
+This module defines a class implementing most methods applicable to SQL
+databases.
+"""
+
 import os
 import sqlite3
 from sage.rings.integer import Integer
@@ -12,6 +19,9 @@ from ..entities.zooentity import ZooEntity
 from ..entities.zooproperty import ZooProperty
 
 class SQLDB(DB):
+    r"""
+    A generic class for SQL databases.
+    """
     db = None
     
     data_string = None
@@ -21,6 +31,7 @@ class SQLDB(DB):
     none_val = 'NULL'
     random = 'RANDOM()'
 
+    # Conversions from Sage/Python to database types
     convert_to = {
         Integer: int,
         Rational: float,
@@ -30,6 +41,7 @@ class SQLDB(DB):
         ZooEntity: int
     }
 
+    # Conversions from database to Sage/Python types
     convert_from = {
         int: Integer,
         float: create_RealNumber,
@@ -40,7 +52,8 @@ class SQLDB(DB):
         bool: bool,
         ZooEntity: Integer
     }
-    
+
+    # SQL type keywords
     types = {
         Integer: 'INTEGER',
         Rational: 'REAL',
@@ -50,12 +63,14 @@ class SQLDB(DB):
         ZooEntity: 'INTEGER'
     }
 
+    # SQL constraint keywords
     constraints = {
         'not_null': 'NOT NULL',
         'primary_key': 'PRIMARY KEY',
         'unique': 'UNIQUE'
     }
 
+    # Symbols for SQL binary operations
     binaryops = {
         query.LessThan: '<',
         query.LessEqual: '<=',
@@ -78,6 +93,9 @@ class SQLDB(DB):
         query.In: 'IN'
     }
 
+    # Symbols for SQL unary operations
+    # The second element of the tuple is ``True`` for prefixes,
+    # ``False`` for suffixes, and ``None`` for functions.
     unaryops = {
         query.Not: ('NOT', True),
         query.Absolute: ('abs', None),
@@ -87,29 +105,74 @@ class SQLDB(DB):
         query.IsNotNull: ('IS NOT NULL', False)
     }
 
+    # SQL logical connectives
     logicalexps = {
         query.And: ' AND ',
         query.Or: ' OR '
     }
 
+    # SQL logical constants
     logicalconsts = {
         query.And: '1',
         query.Or: '0'
     }
 
     def quoteIdent(self, ident):
+        r"""
+        Wrap an identifier into appropriate quotes.
+
+        ``ident`` is wrapped by the character specified by the database's
+        ``ident_quote`` field.
+
+        INPUT:
+
+        - ``ident`` - the identifier to be quoted.
+        """
         return '%s%s%s' % (self.ident_quote, ident, self.ident_quote)
 
     def tableAlias(self, table):
+        r"""
+        Return an alias for the given table.
+
+        If ``table`` is a string, it is returned unchanged.
+
+        INPUT:
+
+        - ``table`` - the ``Table`` to get the alias for.
+        """
         return table.tables[0]['alias'] if isinstance(table, query.Table) \
                else table
 
     def binaryOp(self, op, left, right):
+        r"""
+        Format a SQL binary operation.
+
+        If division is performed, the left argument is converted to the type
+        corresponding to ``Rational`` (usually a floating-point number) to
+        prevent doing integer division.
+
+        INPUT:
+
+        - ``op`` - the binary operator.
+
+        - ``left`` - an SQL string representing the left operand.
+
+        - ``right`` - an SQL string representing the right operand.
+        """
         if isinstance(op, query.Divide):
             left = 'CAST(%s AS %s)' % (left, self.types[Rational])
         return '(%s) %s (%s)' % (left, self.binaryops[op.__class__], right)
 
     def unaryOp(self, op, exp):
+        r"""
+        Format a SQL unary operation.
+
+        INPUT:
+
+        - ``op`` - the unary operator.
+
+        - ``exp`` - an SQL string representing the operand.
+        """
         k, p = self.unaryops[op.__class__]
         if p is True:
             return '%s (%s)' % (k, exp)
@@ -119,6 +182,17 @@ class SQLDB(DB):
             return '%s(%s)' % (k, exp)
 
     def makeType(self, t, c):
+        r"""
+        Format a type and constraint specification.
+
+        INPUT:
+
+        - ``t`` - the Sage/Python type of the represented object. Subclasses of
+          ``ZooEntity`` are represented by a foreign key to the appropriate
+          table.
+
+        - ``c`` - a collection of column constraints.
+        """
         cons = ''.join([' ' + self.constraints[x] for x in c])
         if issubclass(t, ZooEntity):
             return self.types[ZooEntity] + cons + ' REFERENCES %s(%s)' \
@@ -128,6 +202,19 @@ class SQLDB(DB):
             return self.types[t] + cons
 
     def makeTable(self, t, alias = True):
+        r"""
+        Format a table specification.
+
+        Returns a tuple containing an SQL string with wildcards, and a list of
+        objects corresponding to the wildcards.
+
+        INPUT:
+
+        - ``t`` - the ``Table`` to be converted to SQL. If ``t`` is a string,
+          it is returned unchanged.
+
+        - ``alias`` - whether to give the table an alias (default: ``True``).
+        """
         if isinstance(t, query.Table):
             if not alias and len(t.tables) == 1:
                 return self.makeTable(t.tables[0]['table'], alias = False)
@@ -167,6 +254,23 @@ class SQLDB(DB):
             return self.quoteIdent(t), []
 
     def makeExpression(self, exp, alias = False):
+        r"""
+        Format an SQL expression.
+
+        Returns a tuple containing an SQL string with wildcards, and a list of
+        objects corresponding to the wildcards.
+
+        INPUT:
+
+        - ``exp`` - the ``Expression`` to be converted to SQL. If ``exp`` is a
+          ``Table``, an expression for all columns in the table is output. If
+          ``exp`` is an ``All`` object, then the expression for all columns in
+          all tables is output. In the other cases, ``exp`` is a converted into
+          an ``Expression`` using ``query.makeExpression``.
+
+        - ``alias`` - whether to give the expression an alias
+          (default: ``False``).
+        """
         if exp is None:
             return (self.none_val, [])
         elif isinstance(exp, query.All):
@@ -223,22 +327,71 @@ class SQLDB(DB):
             raise NotImplementedError
 
     def cursor(self, **kargs):
+        r"""
+        Return a cursor.
+
+        Any keyword input is forwarded to the Python database interface's
+        ``cursor`` method.
+        """
         return self.db.cursor(**kargs)
 
     def commit(self, **kargs):
+        r"""
+        Commit the active transaction.
+
+        Any keyword input is forwarded to the Python database interface's
+        ``commit`` method.
+        """
         self.db.commit(**kargs)
 
     def rollback(self, **kargs):
+        r"""
+        Rollback the active transaction.
+
+        Any keyword input is forwarded to the Python database interface's
+        ``rollback`` method.
+        """
         self.db.rollback(**kargs)
 
     def handle_exception(self, ex):
+        r"""
+        Clean up after an exception occurs.
+
+        Rolls back the current transaction so that the database will be in a
+        usable state.
+        """
         self.rollback()
         raise ex
 
     def createIndex(self, cur, name, idx):
+        r"""
+        Create an index.
+
+        Since different databases handle index creation differently, this
+        method is not implemented and should be overridden.
+
+        INPUT:
+
+        - ``cur`` - the cursor to be used.
+
+        - ``name`` - the table on which the index is to be created.
+
+        - ``idx`` - a list of columns to be indexed. May also be a tuple
+          containing said list and a collection of constraints.
+        """
         raise NotImplementedError
 
     def init_table(self, spec, commit = False):
+        r"""
+        Create a table if it does not exist.
+
+        INPUT:
+
+        - ``spec`` - table specification (see the ``spec/`` folder).
+
+        - ``commit`` - whether to commit after a new table is created
+          (defaut: ``False``).
+        """
         try:
             pkey = enlist(spec['primary_key'])
             idxs = [k[0] if isinstance(k, tuple) else k
@@ -282,9 +435,29 @@ class SQLDB(DB):
             self.handle_exception(ex)
 
     def returning(self, id):
+        r"""
+        Format a RETURNING expression.
+
+        This is not standard SQL, so this method returns an empty string.
+
+        INPUT:
+
+        - ``id`` - the name of the ID column.
+        """
         return ''
 
     def limit(self, limit = None, offset = None):
+        r"""
+        Format a LIMIT clause.
+
+        An OFFSET clause will only be added if ``limit`` is not ``None``.
+
+        INPUT:
+
+        - ``limit`` - the maximal number of rows to output (default: ``None``).
+
+        - ``offset`` - the number of rows to skip (default: ``None``).
+        """
         out = ''
         if limit is not None:
             out += ' LIMIT %d' % limit
@@ -293,6 +466,26 @@ class SQLDB(DB):
         return out
 
     def insert_row(self, table, row, cur = None, commit = None, id = None):
+        r"""
+        Insert a row into the database.
+
+        Returns the cursor used for inserting.
+
+        INPUT:
+
+        - ``table`` - the table to insert into.
+
+        - ``row`` - a dictionary mapping columns to values to be inserted.
+
+        - ``cur`` - the cursor to be used. If ``None`` (default), a new cursor
+          will be created. If ``False``, a new cursor will also be created, but
+          not returned.
+
+        - ``commit`` - whether to commit after a new row is inserted. If
+          ``None`` (default), commit only if ``cur`` is ``False``.
+
+        - ``id`` - the name of the ID column (default: ``None``).
+        """
         try:
             cols = [c for c in row if row[c] is not None]
             if cur is False:
@@ -326,9 +519,41 @@ class SQLDB(DB):
             self.handle_exception(ex)
 
     def lastrowid(self, cur):
+        r"""
+        Return the ID of the last inserted row.
+
+        Uses the cursor's ``lastrowid`` field.
+
+        INPUT:
+
+        - ``cur`` - the cursor to be used.
+        """
         return cur.lastrowid
 
     def update_rows(self, table, row, cond = False, cur = None, commit = None):
+        r"""
+        Update rows matching specified criteria.
+
+        Returns the cursor used for updating.
+
+        INPUT:
+
+        - ``table`` - the table to be updated.
+
+        - ``row`` - a dictionary mapping columns to values to be set.
+
+        - ``cond`` - an ``Expression`` specifying the condition necessary for
+          the update to take place. If ``False`` (default), a warning will be
+          raised and no update performed. Other values are converted into an
+          ``Expression`` using ``query.makeExpression``.
+
+        - ``cur`` - the cursor to be used. If ``None`` (default), a new cursor
+          will be created. If ``False``, a new cursor will also be created, but
+          not returned.
+
+        - ``commit`` - whether to commit after the rows are updated. If
+          ``None`` (default), commit only if ``cur`` is ``False``.
+        """
         if cond is False:
             raise UserWarning("false condition given; to change all rows specify cond = None")
         if cur is False:
@@ -365,6 +590,27 @@ class SQLDB(DB):
             self.handle_exception(ex)
 
     def delete_rows(self, table, cond = False, cur = None, commit = None):
+        r"""
+        Delete rows matching specified criteria.
+
+        Returns the cursor used for deleting.
+
+        INPUT:
+
+        - ``table`` - the table whose rows are to be deleted.
+
+        - ``cond`` - an ``Expression`` specifying the condition necessary for
+          the deletion to take place. If ``False`` (default), a warning will be
+          raised and no deletion performed. Other values are converted into an
+          ``Expression`` using ``query.makeExpression``.
+
+        - ``cur`` - the cursor to be used. If ``None`` (default), a new cursor
+          will be created. If ``False``, a new cursor will also be created, but
+          not returned.
+
+        - ``commit`` - whether to commit after the rows are deleted. If
+          ``None`` (default), commit only if ``cur`` is ``False``.
+        """
         if cond is False:
             raise UserWarning("false condition given; to delete all rows specify cond = None")
         if cur is False:
@@ -397,6 +643,46 @@ class SQLDB(DB):
     def query(self, columns, table, cond = None, groupby = None,
               orderby = None, limit = None, offset = None, distinct = False,
               cur = None, subquery = False):
+        r"""
+        Perform a query.
+
+        Returns the cursor used for querying.
+
+        INPUT:
+
+        - ``columns`` - a collection of columns to be selected.
+
+        - ``table`` - the ``Table`` to get the data from
+
+        - ``cond`` - an ``Expression`` specifying the condition that the
+          returned rows should satisfy (default: ``None``). Other values are
+          converted into an ``Expression`` using ``query.makeExpression``.
+
+        - ``groupby`` - a collection of expressions to group by. If ``None`
+          (default), no grouping is performed.
+
+        - ``orderby`` - a collection of expressions to sort by. If ``None`
+          (default), no sorting is performed.
+
+        - ``limit`` - the maximal number of rows to output (default: ``None``).
+
+        - ``offset`` - the number of rows to skip (default: ``None``).
+
+        - ``distinct`` - whether to only return distinct rows
+          (default: ``False``).
+
+        - ``cur`` - the cursor to be used. If ``None`` (default), a new cursor
+          will be created. If ``False``, a new cursor will also be created, but
+          not returned.
+
+        - ``commit`` - whether to commit after the rows are updated. If
+          ``None`` (default), commit only if ``cur`` is ``False``.
+
+        - ``subquery`` - whether the query is a subquery of a larger query
+          (default: ``False``). If ``True``, the query will not be executed -
+          instead the method will return a tuple containing an SQL string with
+          wildcards, and a list of objects corresponding to the wildcards.
+        """
         try:
             dist = 'DISTINCT ' if distinct else ''
             cols = [self.makeExpression(col, alias = True) for col in columns]
