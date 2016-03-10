fab2437c877c5d1b84c52ed9c4c177bfc397bb25
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2016-03-10 23:16:12 +0100

    Start documenting entities

--- discretezoo/entities/__init__.py
+++ discretezoo/entities/__init__.py
@@ -1,1 +1,28 @@
+r"""
+Entities module
+
+This module contains the following submodules:
+
+- ``zootypes``: Type initialization functions
+
+- ``zooentity``: A superclass for all DiscreteZOO entities
+
+- ``change``: A class representing changes to the database
+
+- ``zooproperty``: A superclass for properties of DiscreteZOO objects
+
+- ``zoodict``: A dictionary metaclass
+
+- ``zooset``: A set metaclass
+
+- ``zooobject``: A superclass for all DiscreteZOO objects
+
+- ``zoograph``: A class for generic undirected graphs
+
+- ``vt``: A class for vertex-transitive graphs
+
+- ``cvt``: A class for cubic vertex-transitive graphs
+
+- ``spx``: A class for split Praeger-Xu graphs
+"""
 __all__ = ["zooentity", "zoograph", "vt", "cvt", "spx"]
