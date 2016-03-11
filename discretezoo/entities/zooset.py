e53d57d21dc96fc9ce18c4d1b6b05ed48c3c896a
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2016-03-11 02:08:50 +0100

    Add documentation for ZooDict and ZooSet

--- discretezoo/entities/zooset.py
+++ discretezoo/entities/zooset.py
@@ -9,11 +9,39 @@ from ..db.query import enlist
 from ..util.utility import lookup
 
 class _ZooSet(dict, ZooProperty):
+    r"""
+    A generic set class with database interaction.
+
+    Extends Python's ``dict`` class.
+    """
     _parent = None
     _objid = None
     _use_tuples = None
 
     def __init__(self, data, vals = None, **kargs):
+        r"""
+        Object constructor.
+
+        INPUT:
+
+        - ``data`` - a ``_ZooSet`` instance to replicate, or the ID of the
+          object for which properties are being represented.
+
+        - ``vals`` - a set of values to be stored (default: ``None``).
+          Only used if ``store`` is ``True``.
+
+        - ``db`` -- the database being used (must be a named parameter;
+          default: ``None``).
+
+        - ``store`` - whether to store the data in ``vals`` to the database
+          (must be a named parameter; default: ``discretezoo.WRITE_TO_DB``).
+
+        - ``cur`` - the cursor to use for database interaction
+          (must be a named parameter; default: ``None``).
+
+        - ``commit`` - whether to commit the changes to the database
+          (must be a named parameter; default: ``None``).
+        """
         dict.__init__(self)
         self._zooid = False
         if isinstance(data, _ZooSet):
@@ -27,22 +55,27 @@ class _ZooSet(dict, ZooProperty):
             if vals is not None and kargs["store"]:
                 for val in vals:
                     self.add(val, store = True, cur = kargs["cur"])
+                if kargs["commit"]:
+                    self._db.commit()
             else:
                 t = Table(self._spec["name"])
                 cur = self._db.query([t], t, {self._foreign_key: data,
                                               "deleted": False},
                                      cur = kargs["cur"])
-                r = cur.fetchone()
-                while r is not None:
+                for r in cur:
                     v = tuple([r[k] for k in self._ordering])
                     if not self._use_tuples:
                         v = v[0]
                     self[v] = r[self._spec["primary_key"]]
-                    r = cur.fetchone()
-            if kargs["commit"]:
-                self._db.commit()
 
     def __getattr__(self, name):
+        r"""
+        Get the set of values associated to the property with the given name.
+
+        INPUT:
+
+        - ``name`` - the name of the requested property.
+        """
         if name == self._spec["primary_key"]:
             return set(self.values())
         if name not in self._ordering:
@@ -53,12 +86,33 @@ class _ZooSet(dict, ZooProperty):
         return {t[i] for t in self}
 
     def __repr__(self):
+        r"""
+        The object representation of the set.
+        """
         return '{%s}' % ', '.join(sorted(self))
 
     def _unique_index(self):
+        r"""
+        Return a list of columns uniquely determining a row in the database.
+
+        Returns a list of value columns.
+        """
         return self._spec["indices"][0][0]
 
-    def _normalize(self, x, id):
+    def _normalize(self, x, id = None):
+        r"""
+        Normalize a value to the standard form.
+
+        Returns a triple containing the value in the standard and tuple forms
+        and its entry ID.
+
+        INPUT:
+
+        - ``x`` - the value to normalize.
+
+        - ``id`` - the entry ID of the value. If ``None`` (default), it is
+          retrieved from the value.
+        """
         tx = tuple(enlist(x))
         if id is None and len(tx) > len(self._ordering):
             id = tx[0]
@@ -71,6 +125,24 @@ class _ZooSet(dict, ZooProperty):
 
     @staticmethod
     def _get_column(cl, name, table, join = None, by = None):
+        r"""
+        Return a ``ColumnSet`` object for the given class.
+
+        INPUT:
+
+        - ``cl`` - the class to get the object for.
+
+        - ``name`` - the name of the property. Ignored in this context.
+
+        - ``table`` - the table containing the objects whose properties are
+          represented by ``cl``.
+
+        - ``join`` - a join of tables needed to determine the object
+          (default: ``None``).
+
+        - ``by`` - - ``by`` - the criterion to join by (default: ``None``).
+          See ``db.query.Table.join`` for more information.
+        """
         if not isinstance(table, Table):
             table = Table(table)
         if join is not None:
@@ -84,6 +156,21 @@ class _ZooSet(dict, ZooProperty):
             foreign = cl._foreign_key, ordering = cl._ordering)
 
     def add(self, x, id = None, **kargs):
+        r"""
+        Add an element to the set.
+
+        INPUT:
+
+        - ``x`` - the element to add.
+
+        - ``id`` - the ID to use for the entry (default: ``None``).
+
+        - ``store`` - whether to write the data to the database
+          (must be a named parameter; default: ``discretezoo.WRITE_TO_DB``).
+
+        - ``cur`` - the cursor to use for database interaction
+          (must be a named parameter; default: ``None``).
+        """
         store = lookup(kargs, "store", default = discretezoo.WRITE_TO_DB)
         cur = lookup(kargs, "cur", default = None)
         x, tx, id = self._normalize(x, id)
@@ -96,6 +183,17 @@ class _ZooSet(dict, ZooProperty):
         self[x] = id
 
     def clear(self, **kargs):
+        r"""
+        Remove all entries.
+
+        INPUT:
+
+        - ``store`` - whether to delete the data from the database
+          (must be a named parameter; default: ``discretezoo.WRITE_TO_DB``).
+
+        - ``cur`` - the cursor to use for database interaction
+          (must be a named parameter; default: ``None``).
+        """
         store = lookup(kargs, "store", default = discretezoo.WRITE_TO_DB)
         cur = lookup(kargs, "cur", default = None)
         if store:
@@ -103,16 +201,50 @@ class _ZooSet(dict, ZooProperty):
                               cur = cur)
         dict.clear(self)
 
-    def difference(self, other):
-        return set(self).difference(other)
+    def difference(self, *largs):
+        r"""
+        Return the difference of two or more sets as a new set.
 
-    def difference_update(self, other, **kargs):
+        INPUT:
+
+        - any number of collections as unnamed parameters.
+        """
+        return set(self).difference(*largs)
+
+    def difference_update(self, *largs, **kargs):
+        r"""
+        Remove all elements of given sets from this set.
+
+        INPUT:
+
+        - any number of collections as unnamed parameters.
+
+        - ``store`` - whether to delete the data from the database
+          (must be a named parameter; default: ``discretezoo.WRITE_TO_DB``).
+
+        - ``cur`` - the cursor to use for database interaction
+          (must be a named parameter; default: ``None``).
+        """
         store = lookup(kargs, "store", default = discretezoo.WRITE_TO_DB)
         cur = lookup(kargs, "cur", default = None)
-        for x in other:
-            self.discard(x, store = store, cur = cur)
+        for other in largs:
+            for x in other:
+                self.discard(x, store = store, cur = cur)
 
     def discard(self, x, **kargs):
+        r"""
+        Remove an element from a set if it is a member.
+
+        INPUT:
+
+        - ``x`` - the element to remove.
+
+        - ``store`` - whether to delete the data from the database
+          (must be a named parameter; default: ``discretezoo.WRITE_TO_DB``).
+
+        - ``cur`` - the cursor to use for database interaction
+          (must be a named parameter; default: ``None``).
+        """
         store = lookup(kargs, "store", default = discretezoo.WRITE_TO_DB)
         cur = lookup(kargs, "cur", default = None)
         try:
@@ -120,26 +252,85 @@ class _ZooSet(dict, ZooProperty):
         except KeyError:
             pass
 
-    def intersection(self, other):
-        return set(self).intersection(other)
+    def intersection(self, *largs):
+        r"""
+        Return the intersection of two or more sets as a new set.
 
-    def intersection_update(self, other, **kargs):
+        INPUT:
+
+        - any number of collections as unnamed parameters.
+        """
+        return set(self).intersection(*largs)
+
+    def intersection_update(self, *largs, **kargs):
+        r"""
+        Update a set with the intersection of itself and given sets.
+
+        INPUT:
+
+        - any number of collections as unnamed parameters.
+
+        - ``store`` - whether to delete the data from the database
+          (must be a named parameter; default: ``discretezoo.WRITE_TO_DB``).
+
+        - ``cur`` - the cursor to use for database interaction
+          (must be a named parameter; default: ``None``).
+        """
         store = lookup(kargs, "store", default = discretezoo.WRITE_TO_DB)
         cur = lookup(kargs, "cur", default = None)
-        for x in self:
-            if x not in other:
+        for x in set(self):
+            if not all(x in other for other in largs):
                 self.remove(x, store = store, cur = cur)
 
     def isdisjoint(self, other):
+        r"""
+        Return ``True`` if two sets have an empty intersection.
+
+        INPUT:
+
+        - ``other`` - the set to check whether it is disjoint with this set.
+        """
         return set(self).isdisjoint(other)
 
     def issubset(self, other):
+        r"""
+        Report whether another set contains this set.
+
+        INPUT:
+
+        - ``other`` - the set to check whether this set is its subset.
+        """
         return set(self).issubset(other)
 
     def issuperset(self, other):
+        r"""
+        Report whether this set contains another set.
+
+        INPUT:
+
+        - ``other`` - the set to check whether this set is its superset.
+        """
         return set(self).issuperset(other)
 
     def pop(self, *largs, **kargs):
+        r"""
+        Remove and return an arbitrary set element.
+
+        Raises ``KeyError`` if the set is empty.
+
+        INPUT:
+
+        - an unnamed parameter specifies the set element to pop.
+
+        - a second unnamed parameter specifies the value to return if the
+          first parameter does not exist in the set.
+
+        - ``store`` - whether to delete the data from the database
+          (must be a named parameter; default: ``discretezoo.WRITE_TO_DB``).
+
+        - ``cur`` - the cursor to use for database interaction
+          (must be a named parameter; default: ``None``).
+        """
         store = lookup(kargs, "store", default = discretezoo.WRITE_TO_DB,
                        destroy = True)
         cur = lookup(kargs, "cur", default = None, destroy = True)
@@ -160,6 +351,20 @@ class _ZooSet(dict, ZooProperty):
                 raise ex
 
     def popitem(self, **kargs):
+        r"""
+        Remove and return some (value, ID) pair, where the ID is the ID of the
+        entry.
+
+        Raise ``KeyError`` if the set is empty.
+
+        INPUT:
+
+        - ``store`` - whether to delete the data from the database
+          (must be a named parameter; default: ``discretezoo.WRITE_TO_DB``).
+
+        - ``cur`` - the cursor to use for database interaction
+          (must be a named parameter; default: ``None``).
+        """
         store = lookup(kargs, "store", default = discretezoo.WRITE_TO_DB)
         cur = lookup(kargs, "cur", default = None)
         k, v = dict.popitem(self)
@@ -173,6 +378,24 @@ class _ZooSet(dict, ZooProperty):
         return (k, v)
 
     def remove(self, x = None, id = None, **kargs):
+        r"""
+        Remove an element from a set.
+
+        If the element is not a member, raise a ``KeyError``.
+
+        INPUT:
+
+        - ``x`` - the element to remove (default: ``None``).
+
+        - ``id`` - the ID of the entry to delete (default: ``None``). Only used
+          if ``x`` is ``None``.
+
+        - ``store`` - whether to delete the data from the database
+          (must be a named parameter; default: ``discretezoo.WRITE_TO_DB``).
+
+        - ``cur`` - the cursor to use for database interaction
+          (must be a named parameter; default: ``None``).
+        """
         store = lookup(kargs, "store", default = discretezoo.WRITE_TO_DB)
         cur = lookup(kargs, "cur", default = None)
         if x is None:
@@ -193,6 +416,27 @@ class _ZooSet(dict, ZooProperty):
         del self[x]
 
     def rename(self, old, new = None, id = None, **kargs):
+        r"""
+        Replace a set member with another value.
+
+        If the new value already is a member, raise a ``KeyError``.
+
+        INPUT:
+
+        - ``old`` - the element to replace.
+
+        - ``new`` - the new value. If ``None`` (default), treat ``old`` as the
+          new value and replace the element identified by ``id``.
+
+        - ``id`` - the ID of the entry to replace (default: ``None``). Only
+          used if ``new`` is ``None``.
+
+        - ``store`` - whether to change the data in the database
+          (must be a named parameter; default: ``discretezoo.WRITE_TO_DB``).
+
+        - ``cur`` - the cursor to use for database interaction
+          (must be a named parameter; default: ``None``).
+        """
         store = lookup(kargs, "store", default = discretezoo.WRITE_TO_DB)
         cur = lookup(kargs, "cur", default = None)
         if new is None:
@@ -223,9 +467,29 @@ class _ZooSet(dict, ZooProperty):
         self[new] = id
 
     def symmetric_difference(self, other):
+        r"""
+        Return the symmetric difference of two sets as a new set.
+
+        INPUT:
+
+        - ``other`` - the set to make the symmetric difference with.
+        """
         return set(self).symmetric_difference(other)
 
     def symmetric_difference_update(self, other, **kargs):
+        r"""
+        Update a set with the symmetric difference of itself and another.
+
+        INPUT:
+
+        - ``other`` - the set to make the symmetric difference with.
+
+        - ``store`` - whether to update the data in the database
+          (must be a named parameter; default: ``discretezoo.WRITE_TO_DB``).
+
+        - ``cur`` - the cursor to use for database interaction
+          (must be a named parameter; default: ``None``).
+        """
         store = lookup(kargs, "store", default = discretezoo.WRITE_TO_DB)
         cur = lookup(kargs, "cur", default = None)
         if not isinstance(other, dict):
@@ -236,19 +500,57 @@ class _ZooSet(dict, ZooProperty):
             else:
                 self.add(x, other[x], store = store, cur = cur)
 
-    def union(self, other):
-        return set(self).union(other)
+    def union(self, *largs):
+        r"""
+        Return the union of sets as a new set.
 
-    def update(self, other, **kargs):
+        INPUT:
+
+        - any number of collections as unnamed parameters.
+        """
+        return set(self).union(*largs)
+
+    def update(self, *largs, **kargs):
+        r"""
+        Update a set with the union of itself and others.
+
+        INPUT:
+
+        - any number of collections as unnamed parameters.
+
+        - ``store`` - whether to store the data to the database
+          (must be a named parameter; default: ``discretezoo.WRITE_TO_DB``).
+
+        - ``cur`` - the cursor to use for database interaction
+          (must be a named parameter; default: ``None``).
+        """
         store = lookup(kargs, "store", default = discretezoo.WRITE_TO_DB)
         cur = lookup(kargs, "cur", default = None)
         if not isinstance(other, dict):
             other = {x: None for x in other}
-        for x in other:
-            if x not in self or self[x] is None:
-                self.add(x, other[x], store = store, cur = cur)
+        for other in largs:
+            for x in other:
+                if x not in self or self[x] is None:
+                    self.add(x, other[x], store = store, cur = cur)
 
 def ZooSet(parent, name, spec, use_tuples = None):
+    r"""
+    Construct a subclass of ``_ZooDict``.
+
+    INPUT:
+
+    - ``parent`` - the class whose property is being represented.
+
+    - ``name`` - the name of the represented property.
+
+    - ``spec`` - metaclass specification. The ``"params"`` field should contain
+      a dictionary with the specification for value properties at key
+      ``"fields"``. See the documentation in the ``spec/`` folder for more
+      details.
+
+    - ``use_tuples`` - whether to use tuples for value properties. If ``None``
+      (default), use tuples when the number of value properties is not one.
+    """
     fields = spec["params"]["fields"]
     if len(fields) != 1:
         use_tuples = True
@@ -256,15 +558,18 @@ def ZooSet(parent, name, spec, use_tuples = None):
         use_tuples = False
     id = "zooid"
     fkey = "%s_id" % parent._spec["name"]
-
-    class ZooSet(_ZooSet):
-        _use_tuples = use_tuples
-        _ordering = sorted(fields.keys())
-        _foreign_key = fkey
-        _foreign_obj = parent
-        _spec = {
+    clsdict = {
+        '__doc__': r"""
+    Set object for the property ``%s`` of the class ``%s``.
+    """ % (name, parent.__name__),
+        '_use_tuples': use_tuples,
+        '_ordering': sorted(fields.keys()),
+        '_foreign_key': fkey,
+        '_foreign_obj': parent,
+        '_spec': {
             "name": "%s_%s" % (parent._spec["name"], name),
             "primary_key": id,
+            "indices": [([fkey] + fields.keys(), {"unique"})],
             "skip": {fkey, "deleted"},
             "fields": {
                 id: ZooEntity,
@@ -278,9 +583,10 @@ def ZooSet(parent, name, spec, use_tuples = None):
             "compute": {},
             "default": {}
         }
+    }
 
-    ZooSet._spec["fields"].update(fields)
-    ZooSet._spec["indices"] = [([fkey] + fields.keys(), {"unique"})]
+    clsdict["_spec"]["fields"].update(fields)
+    ZooSet = type("ZooSet", (_ZooSet,), clsdict)
     ZooSet._init_spec(ZooSet, spec)
     return ZooSet
 
